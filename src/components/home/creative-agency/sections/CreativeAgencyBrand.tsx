2f3d1c979132f6381aa50e1cf2aab88eee37a799
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-08-11 02:57:04 +0300

    edit

--- src/components/home/creative-agency/sections/CreativeAgencyBrand.tsx
+++ src/components/home/creative-agency/sections/CreativeAgencyBrand.tsx
@@ -2,38 +2,51 @@
 import { useIsDarkRoute } from "@/hooks/useIsDarkRoute";
 import Image from "next/image";
 
-const brandLogosDark = [
-    { img: "/assets/img/brand/clients/abdi-ibrahim.png", width: 270, height: 64 },
-    { img: "/assets/img/brand/clients/puttikao-health.jpeg", width: 175, height: 70 },
-    { img: "/assets/img/brand/clients/fokur-logo.png", width: 190, height: 55 },
-    { img: "/assets/img/brand/clients/gent-logo.png", width: 160, height: 65 },
-    { img: "/assets/img/brand/clients/goddess-logo.png", width: 230, height: 85 },
-    { img: "/assets/img/brand/clients/obur-logo.png", width: 250, height: 75 },
-];
-const brandLogosWhite = [
-    { img: "/assets/img/brand/clients/abdi-ibrahim.png", width: 270, height: 64 },
-    { img: "/assets/img/brand/clients/puttikao-health.jpeg", width: 175, height: 70 },
-    { img: "/assets/img/brand/clients/fokur-logo.png", width: 190, height: 55 },
-    { img: "/assets/img/brand/clients/gent-logo.png", width: 160, height: 65 },
-    { img: "/assets/img/brand/clients/goddess-logo.png", width: 230, height: 85 },
-    { img: "/assets/img/brand/clients/obur-logo.png", width: 250, height: 75 },
+const brandLogos = [
+    { img: "/assets/img/brand/clients/abdi-ibrahim.png", name: "Abdi İbrahim" },
+    { img: "/assets/img/brand/clients/puttikao-health.jpeg", name: "Puttikao Health" },
+    { img: "/assets/img/brand/clients/fokur-logo.png", name: "Fokur" },
+    { img: "/assets/img/brand/clients/gent-logo.png", name: "Gent" },
+    { img: "/assets/img/brand/clients/goddess-logo.png", name: "Goddess" },
+    { img: "/assets/img/brand/clients/obur-logo.png", name: "Obur" },
 ];
 
 const CreativeAgencyBrand = () => {
 	const isDarkTheme = useIsDarkRoute();
-	const brands = isDarkTheme ? brandLogosWhite : brandLogosDark
 
 	return (
 		<div className="px-brand-area px-brand-style-5 pt-120 pb-120">
 			<div className="container container-1550">
-				<div className="row gx-0 align-items-center text-center">
-					{brands.map((brand, index) => (
+				<div className="row gx-0 align-items-stretch text-center">
+					{brandLogos.map((brand, index) => (
 						<div
 							key={brand.img}
 							className="col-xl-2 col-lg-4 col-md-6 col-sm-6"
 						>
-							<div className="px-brand-item" style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "80px", padding: "10px" }}>
-								<Image className="img-fluid" width={brand.width} height={brand.height} style={{ width: "auto", height: "auto", maxWidth: `${brand.width}px`, maxHeight: `${brand.height}px`, objectFit: "contain" }} src={brand.img} alt={`brand-${index + 1}`} />
+							<div
+								className="px-brand-item"
+								style={{
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+									height: "100%",
+									minHeight: "100px",
+									padding: "20px 25px",
+								}}
+							>
+								<Image
+									width={200}
+									height={60}
+									src={brand.img}
+									alt={brand.name}
+									style={{
+										width: "100%",
+										height: "auto",
+										maxWidth: "140px",
+										maxHeight: "45px",
+										objectFit: "contain",
+									}}
+								/>
 							</div>
 						</div>
 					))}
