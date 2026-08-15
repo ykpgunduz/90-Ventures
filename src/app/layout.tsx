bb069abfead095b505768e83847ba08d7c8757dc
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-08-15 13:20:18 +0300

    seo-google-revize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -240,7 +240,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src="https://www.googletagmanager.com/ns.html?id=GTM-5VX7CKS4"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
